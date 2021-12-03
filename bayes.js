b796bf01d07c1054f26926b89e9763fb6be4aae6
Dante <dante_padley@yahoo.com>
2021-12-02 16:06:50 -0800

    completed first few functions of bayes classification

--- bayes.js
+++ bayes.js
@@ -1,9 +1,96 @@
 let  functions =  require('./functions.js');
 console.log(functions.get50());
 console.log(functions.get20());
+const csv = require('csv-parser');
+const fs = require('fs');
 
-class bayes {
+let hamCount = 0;
+let spamCount = 0;
+let unorthodoxCount = [0, 0];
+let capitalsCount = [0, 0];
+let linksCount = [0, 0];
+let topEight = [
+    ['',0],['',0],['',0],['',0],['',0],['',0],['',0],['',0]
+];
 
+function checkContent(row){
+    let content = row.content;
+    let spamvalue = row.spamvalue;
+    if(functions.unorthodoxCharacters(content)){
+        if (spamvalue === 'ham'){
+            unorthodoxCount[0] = unorthodoxCount[0] + 1;
+        }
+        else if (spamvalue === 'spam'){
+            unorthodoxCount[1] = unorthodoxCount[1] + 1;
+        } 
+    }
+    if(functions.lotsOfCapitals(content)){
+        if (spamvalue === 'ham'){
+            capitalsCount[0] = capitalsCount[0] + 1;
+        }
+        if (spamvalue === 'spam'){
+            capitalsCount[1] = capitalsCount[1] + 1;
+        }
+    }
+    if(functions.containsLinks(content)){
+        if (spamvalue === 'ham'){
+            linksCount[0] = linksCount[0] + 1;
+        }
+        else if (spamvalue === 'spam'){
+            linksCount[1] = linksCount[1] + 1;
+        }
+    }
 }
 
-module.exports = bayes;
+function checkRow(row){
+    //console.log(row.spamvalue);
+    if(row.spamvalue === 'ham'){
+        hamCount = hamCount + 1;
+    }
+    else if(row.spamvalue === 'spam'){
+        spamCount = spamCount + 1;
+    }
+    checkContent(row);
+}
+
+function topSpamWords(spamString){
+    let spamArray = spamString.split(" ");
+    spamArray.sort();
+    //currentWord
+    /*for(word of spamArray){
+        //if word is same as previous word, add 1
+        //if word is different from previous word, 
+        //compare that word/count to array, and place it if necessary
+        //reset running word/count
+    }*/
+}
+
+function bayes() {
+    let allSpamString = "";
+    fs.createReadStream('spam.csv')
+    .pipe(csv())
+    .on('data', (row) => {
+        checkRow(row);
+        //console.log(row);
+        if (row.spamvalue === 'spam'){
+            allSpamString = allSpamString + " " + row.content;
+        }
+        
+    })
+    .on('end', () => {
+        topSpamWords(allSpamString);
+        console.log('CSV File successfully processed');
+        console.log("Ham: " + hamCount);
+        console.log("Spam: " + spamCount);
+        console.log("Unorthodox Characters in ham: " + unorthodoxCount[0]);
+        console.log("Unorthodox Characters in spam: " + unorthodoxCount[1]);
+        console.log("Too many capital Characters in ham: " + capitalsCount[0]);
+        console.log("Too many capital Characters in spam: " + capitalsCount[1]);
+        console.log("Link in ham: " + linksCount[0]);
+        console.log("Link in spam: " + linksCount[1]);
+        
+    });
+    
+}
+bayes();
+//module.exports = bayes;
--- functions.js
+++ functions.js
@@ -10,41 +10,67 @@ exports.get20 = function get20() {
 /*
 Contains ANY unorthodox characters. Just anything you cant find on a normal english keyboard. Ignore emojis
 */
- exports.unorthodoxCharacters = function unorthodoxCharacters() {
-    return "20";
+ exports.unorthodoxCharacters = function unorthodoxCharacters(content) {
+    for(let i = 0; i < content.length; i++){
+        if (content.charCodeAt(i) > 126){
+            //console.log(content.charCodeAt(i));
+            return true
+        }
+    }
+    return false;
 }
 
 
 /*
 Contains lots of capital lets. Maybe 50% to start? Only valid for strings longer than 30 characters
 */
-exports.lotsOfCapitals = function lotsOfCapitals() {
-    return "20";
+exports.lotsOfCapitals = function lotsOfCapitals(content) {
+    if (content.length >= 30) {
+        let capCount = 0;
+        for (let i = 0; i < content.length; i++) {
+            ch = content.charAt(i);
+            if (isNaN(ch * 1) && ch == ch.toUpperCase()) {
+                capCount = capCount + 1;
+            }
+        }
+        if (capCount > (content.length / 2)) {
+            //console.log(content);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else{
+        return false;
+    }
 }
 
 /*
-Contains lots of capital lets. Maybe 50% to start? Only valid for strings longer than 30 characters
+Contains strings that indicate URLs
 */
-exports.containsLinks = function containsLinks() {
-    return "20";
+exports.containsLinks = function containsLinks(content) {
+
+    if (content.includes("http") || content.includes("www")) {
+        //console.log(content);
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 /*
 Contains a lot of suspicious words like "congratulations or YOU WON"
 */
 exports.suspiciousWords = function suspiciousWords() {
+    //maybe we take this opportunity to find the words with the highest frequency in spam messages
+    //maybe top 8?
     return "20";
 }
 
 /*
 Contains an exceptionally large amount of punctuation for a message
-*/
-exports.lotsOfPunctuation = function lotsOfPunctuation() {
-    return "20";
-}
-
-/*
-Contains an exceptionally large amount of punctuation for a message (Only valid for longer strings)
 */
 exports.lotsOfPunctuation = function lotsOfPunctuation() {
     return "20";
