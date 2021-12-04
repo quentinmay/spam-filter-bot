88d01faed119cfcfd803fabab5a24b824526a827
quentinmay <quentinemay@gmail.com>
2021-12-03 17:07:11 -0800

    Updated tests and added some more cases.

--- test.js
+++ test.js
@@ -22,6 +22,7 @@ let test_strings = ["Empty test",
 
 let testCases = [
     ["Empty test", false],
+    ["test", false],
     ["league?", false],
     ["wanna play some video games", false],
     ["were on discord lol", false],
@@ -37,16 +38,21 @@ let testCases = [
     ["i just won my last game :)", false],
     ["LOL thats sick bro", false],
     ["go check out my youtube video https://www.youtube.com/watch?v=dQw4w9WgXcQ", false],
-
+    ["!@#!@!#@", false],
+    ["", false],
 
     ["congratulations You are a winner, claim THIS FREE prize", true],
     [" sus numbers 858 2222 and characters €ƒŒ", true],
-    ["suspicious numbers 858 dot 126 k 4343", true],
-    ["Here's scummy string  €ƒŒ", true],
+    ["FREE XBOX HERE: https://aksmdkjfm.com/ 12312312312", true],
+    ["FANX8GRY6PWZ  free stuff", true],
     ["BUY MY PRODUCT FREE:", true],
     ["DOWNLOAD MY LINK FREE http://scamlink.com", true],
     ["IMPORTANT!! DOWNLOAD THAT FILE AND YOUR FILES WILL BE PROTECTED!!!!!!!!", true],
-    ["dm me right now for free discord nitro", true]
+    ["dm me right now for free discord nitro", true],
+    ["And code 64USB3 for a free 64GB thumb drive with it", true],
+    ["http://€ƒAaaAAAFJWAOWBNKEBŒA&", true],
+
+
 ];
 
 
@@ -91,7 +97,7 @@ function testTestCases() {
         if (spam != dataSpam) {
             miss++;
         }
-        structDatas.push({ "Message": string.substring(0, 50) + ((string.length > 50) ? "..." : ""), "P(Spam | Conditions))": tableResult["spamProb"].toPrecision(4), "P(Ham | Conditions))": tableResult["hamProb"].toPrecision(4), "Success?": spam == dataSpam })
+        structDatas.push({ "Message": string.substring(0, 50) + ((string.length > 50) ? "..." : ""), "P(Spam | Conditions)": tableResult["spamProb"].toPrecision(4), "P(Ham | Conditions)": tableResult["hamProb"].toPrecision(4), "Success?": spam == dataSpam })
 
     }
     console.log("---------------PREMADE TEST CASES---------------")
